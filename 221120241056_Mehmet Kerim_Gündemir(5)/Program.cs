a32bcad367e7ea4ea072e4c15e1c9bb54cab685a
Mehmet Kerim Gündemir <gundemirkerim9@gmail.com>
2024-11-21 02:04:26 +0300

    Proje dosyası ekle.

---
+++ 221120241056_Mehmet Kerim_Gündemir(5)/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _221120241056_Mehmet_Kerim_Gündemir_5_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int icaci1;
+            Console.WriteLine("Birinci açıyı giriniz");
+            icaci1 = Convert.ToInt16(Console.ReadLine());
+            int icaci2;
+            Console.WriteLine("İkinci açıyı giriniz");
+            icaci2 = Convert.ToInt16(Console.ReadLine());   
+            int icaci3;
+            Console.WriteLine("Üçüncü açıyı giriniz");
+            icaci3 = Convert.ToInt16(Console.ReadLine());
+
+            if (icaci1 + icaci2 + icaci3 < 180)
+            {
+                Console.WriteLine("Bu bir üçgen değil");
+
+            }
+            else if (icaci1 + icaci2 + icaci3 > 180)
+            {
+                Console.WriteLine("Bu bir üçgen değil");
+            }
+            else if (icaci1 + icaci2 + icaci3 == 180)
+            {
+                Console.WriteLine("Bu bir üçgen");
+            }
+            Console.Read();
+           
+
+
+
+            
+
+            
+            
+            
+
+        }
+    }
+}
